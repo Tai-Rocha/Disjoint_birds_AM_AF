ea216cf7e8cc40ac387e69e738a665f06940aff0
Tai-Rocha <taina013@gmail.com>
2020-07-29 11:29:20 -0300

    in progress

---
+++ scripts/Get_Occurrence_Data/e-bird_get_occurrence.R
@@ -0,0 +1,15 @@
+##########################################
+## Get occurence data from e-birds
+## author: Tainá Rocha
+## date: 
+##########################################
+
+
+##Librarys
+library(auk)
+library(raster)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(tigris)
+library(viridisLite)
