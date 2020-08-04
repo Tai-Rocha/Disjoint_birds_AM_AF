8958a95f15b9fcd2456b00581642d6fc4d8eba2b
Tai-Rocha <taina013@gmail.com>
2020-08-04 19:12:39 -0300

    get occurrence from speciesLink through rocc package

---
+++ scripts/Occurrence_data/Get_occurrence/rocc_get_occurrence.R
@@ -0,0 +1,32 @@
+#######################################
+## GEt occurence from speciesLink
+## rocc package (Mortara, S)
+## Author: Tainá Rocha
+#######################################
+
+## Library
+
+library(rocc)
+
+## Get occurrence for all species 
+
+  rspeciesLink(
+  dir = "./data/",
+  filename = "all_speciesLink_raw",
+  save = TRUE,
+  basisOfRecord = c("PreservedSpecimen", "HumanObservation", "MachineObservation"),
+  species = c ("Schiffornis turdina", "Xiphorhynchus guttatus", "Lipaugus vociferans"),
+  collectionCode = NULL,
+  country = NULL,
+  stateProvince = NULL,
+  county = NULL,
+  Coordinates = "Yes",
+  CoordinatesQuality = "Good",
+  Scope = "animals",
+  Synonyms = "species2000",
+  Typus = FALSE,
+  Images = NULL,
+  RedList = FALSE,
+  MaxRecords = NULL
+)
+
