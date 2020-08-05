fc98a362961eb6c462f2935aeae565b5136efc65
Tai-Rocha <taina013@gmail.com>
2020-08-04 23:07:34 -0300

    in progress

--- scripts/Occurrence_data/Get_occurrence/Compile_occ_1.R
+++ scripts/Occurrence_data/Get_occurrence/Compile_occ_1.R
@@ -3,9 +3,14 @@
 ## Auhtor: Tainá Rocha
 #####################################
 
+## Read and load tables 
 
 df1 <- read.table("./data/X_guttatus/sp_records/RAW/inat_raw/xg_inat_raw.txt", sep = " ")
 
 df2 <- read.table("./data/X_guttatus/sp_records/RAW/idigbio_raw/xg_idigbio_raw.txt", sep = " ")
+
+
+## Merge Two Data Frames
+# Merge two data frames by common columns or row names, or do other versions of database join operations.
 
 output <- merge(df2,df1,by=c('name', 'longitude', 'latitude','date'),all=T)
